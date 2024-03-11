bc91b883424f13554d59ba97c813ae57ae32770b
Erik Veenstra <zerikveenstraz@gmail.com>
2024-03-11 22:33:02 +0100

    Add project files.

---
+++ SerialCommunicator/Program.cs
@@ -0,0 +1,34 @@
+using ElectronNET.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseElectron(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+await app.StartAsync();
+
+await Electron.WindowManager.CreateWindowAsync();
+
+app.WaitForShutdown();
