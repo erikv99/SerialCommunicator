661e32e4323adcabee6ac8c4b47a44a9726486b8
Erik Veenstra <zerikveenstraz@gmail.com>
2024-03-23 01:02:18 +0100

    Add mockup data, work on design etc

--- SerialCommunicator/Controllers/CommunicatorController.cs
+++ SerialCommunicator/Controllers/CommunicatorController.cs
@@ -2,44 +2,81 @@
 using Microsoft.Extensions.Options;
 using SerialCommunicator.Models;
 
-namespace SerialCommunicator.Controllers
+namespace SerialCommunicator.Controllers;
+
+public class CommunicatorController : Controller
 {
-    public class CommunicatorController : Controller
+    private readonly List<Command> _commands;
+    private readonly SerialCommunicatorService _serialCommunicatorService;
+
+    public CommunicatorController(
+        IOptions<CommandOptions> commandSettings,
+        SerialCommunicatorService serialCommunicatorService)
     {
-        private readonly List<Command> _commands;
-        private readonly SerialCommunicatorService _serialCommunicatorService;
+        _commands = commandSettings.Value?.Commands ?? _createMockCommands();// new List<Command>();
 
-        public CommunicatorController(
-            IOptions<CommandOptions> commandSettings, 
-            SerialCommunicatorService serialCommunicatorService) 
-        {
-            _commands = commandSettings.Value?.Commands ?? new List<Command>();
-            _serialCommunicatorService = serialCommunicatorService;
-        }
+        _serialCommunicatorService = serialCommunicatorService;
+    }
 
-        public IActionResult Index()
+    private List<Command> _createMockCommands()
+    {
+        return new List<Command>
         {
-            var model = new CommunicatorVM 
+            new Command
             {
-                Commands = _commands
-            };
-
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult RunCommand(int id) 
-        {
-            var command = _commands.FirstOrDefault(c => c.Id == id);
-
-            if (command == null) 
+                Id = 1,
+                Name = "Command 1",
+                Description = "Description for Command 1",
+                Payload = new byte[]
+                {
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08
+                }
+            },
+            new Command
             {
-                return NotFound();
+                Id = 2,
+                Name = "Command 2",
+                Description = "Description for Command 2",
+                Payload = new byte[]
+                {
+                    0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10
+                }
+            },
+            new Command
+            {
+                Id = 3,
+                Name = "Command 3",
+                Description = "Description for Command 3",
+                Payload = new byte[]
+                {
+                    0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18
+                }
             }
+        };
+    }
 
-            var result = _serialCommunicatorService.SendCommand(command);
+    public IActionResult Index()
+    {
+        var model = new CommunicatorVM 
+        {
+            Commands = _commands
+        };
 
-            return Json(new { success = true, response = result });
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult RunCommand(int id) 
+    {
+        var command = _commands.FirstOrDefault(c => c.Id == id);
+
+        if (command == null) 
+        {
+            return NotFound();
         }
+
+        var result = _serialCommunicatorService.SendCommand(command);
+
+        return Json(new { success = true, response = result });
     }
 }
--- SerialCommunicator/Program.cs
+++ SerialCommunicator/Program.cs
@@ -8,11 +8,13 @@ builder.WebHost.UseElectron(args);
 _configureServices(builder.Services);
 
 builder.Configuration
-    .AddJsonFile("commands.json", optional: true, reloadOnChange: true)
-    .AddJsonFile($"commands.local.json", optional: true, reloadOnChange: true);
+    .AddJsonFile("commands.json", optional: true, reloadOnChange: false)
+    .AddJsonFile($"commands.local.json", optional: true, reloadOnChange: false);
 
 // TODO: Make this more elegent and move to own method
-builder.Services.Configure<CommandOptions>(builder.Configuration.GetSection("commands"));
+// TODO: Fix CommandOptions not loading from JSON file for some reason.
+builder.Services.Configure<CommandOptions>(builder.Configuration.GetSection("Commands"));
+//builder.Services.Configure<CommandsOptions>(builder.Configuration.GetSection("Commands"));
 builder.Services.Configure<SerialPortOptions>(builder.Configuration.GetSection("SerialPortOptions"));
 
 var app = builder.Build();
